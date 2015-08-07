7c29839d4c732d33391a70d0ee819277b77b49a4
Maciej Paprocki <maciej.paprocki@bureau-va.com>
2015-08-07 18:09:11 +0100

    added template support

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 var _ = require('lodash');
 var defaults = require('defaults');
+var async = require('async');
 
-var BaseCSS = function(options,version){
+var BaseCSS = function(options,extension){
     this.options = options;
-    this.templatePath = [__dirname];
+    this.ext = extension;
+    this.templatePaths = [__dirname];
 
 }
 
-BaseCSS.prototype.getTemplate(name,vari){
+BaseCSS.prototype.getTemplate = function(name,extension,callback){
+    var self = this;
+    var templateList = _.map(this.templatePaths,function(value){
+        return self.createPath(value,name,extension);
+    })
+
+    async.filter(templateList, fs.exists, function(results){
+    
+    });
 
 }
-BaseCSS.addTemplate.getTemplate()
+BaseCSS.prototype.createPath = function(dir,name,extension) {
 
-BaseCSS.prototype.toString(){
+}
+BaseCSS.prototype.addTemplatePath= function(dir){
+    if(!_.isString){
+        throw new Error('Function addTemplatePath expects first attribute to be string');
+    }else{  
+        this.templatePaths.push(dir);
+    }
+}
+
+BaseCSS.prototype.toString = function(){
 
 }
 
-module.exports = function(options,version){
+module.exports = function(options,extension){
 
-    if(_.isUndefined(version)){
-        version  = 'css';
-    }else if(!_.isString(version)){
+    if(_.isUndefined(extension)){
+        extension  = 'css';
+    }else if(!_.isString(extension)){
         throw new Error('second argument should be string, try "css" ;)');
     }
     if(!_.isObject(options)){
-        return new BaseCSS(options,version);
+        return new BaseCSS(options,extension);
     }
 }   
