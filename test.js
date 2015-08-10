ffb33b0cc9357feb0f3308e79eeae8d892c13820
Maciej Paprocki <maciej.paprocki@bureau-va.com>
2015-08-10 15:01:09 +0100

    added more tests

--- test.js
+++ test.js
@@ -1,10 +1,31 @@
 var base = require('./index');
 
 base({
-	padding:{
-		small:'10px',
-		big:'20px'
-	}
-},'css4').toString(function(result){
-	console.log(result);
+	data:{
+        brakepoint:{
+            sm:'40em',
+            md:'52em',
+            lg:'64em'
+        },
+        padding:{
+    		0:'10px',
+    		1:'20px',
+            bp:{
+                sm:function(value){
+                        return (value.replace(/px$/g,'') * 1.5)+'px';
+                }
+            }
+    	},
+        margin:{
+            small:'10px',
+            bp:{
+                sm:function(value){
+                        return (value.replace(/px$/g,'') * 1.5)+'px';
+                }
+            }
+        }
+
+    }
+},'css4').toString(function(err,result){
+	console.log(result.join(''));
 });
