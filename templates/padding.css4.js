b677507e1d2e2f2682c30443635733648eb28540
Maciej Paprocki <maciej.paprocki@bureau-va.com>
2015-08-17 22:50:28 +0100

    added responsive base padding/margin

--- templates/padding.css4.js
+++ templates/padding.css4.js
@@ -7,13 +7,23 @@ module.exports = require('lodash').template(
     "<%});%>" +
     "}" +
     "<%}%>\n" +
-    //because
+    //as we are using responsive padding. If padding is running with index we render it once without index. 
     "<% _.each(data,function(value,index){%>" +
-    ".<%=resp%>p<%=index%>{ padding:<%=transform(value)%>}\n" +
-    ".<%=resp%>pt<%=index%>{ padding-top:<%=transform(value)%>}\n" +
-    ".<%=resp%>pb<%=index%>{ padding-bottom:<%=transform(value)%>}\n" +
-    ".<%=resp%>pl<%=index%>{ padding-left:<%=transform(value)%>}\n" +
-    ".<%=resp%>pr<%=index%>{ padding-right:<%=transform(value)%>}\n" +
-    ".<%=resp%>py<%=index%>{ padding-top:<%=transform(value)%>; padding-bottom:<%=transform(value)%>}\n" +
-    ".<%=resp%>px<%=index%>{ padding-left: <%=transform(value)%>; padding-right:<%=transform(value)%>}\n" +
+        ".<%=resp%>p<%=index%>{ padding:<%=transform(value)%>}\n" +
+        ".<%=resp%>pt<%=index%>{ padding-top:<%=transform(value)%>}\n" +
+        ".<%=resp%>pb<%=index%>{ padding-bottom:<%=transform(value)%>}\n" +
+        ".<%=resp%>pl<%=index%>{ padding-left:<%=transform(value)%>}\n" +
+        ".<%=resp%>pr<%=index%>{ padding-right:<%=transform(value)%>}\n" +
+        ".<%=resp%>py<%=index%>{ padding-top:<%=transform(value)%>; padding-bottom:<%=transform(value)%>}\n" +
+        ".<%=resp%>px<%=index%>{ padding-left: <%=transform(value)%>; padding-right:<%=transform(value)%>}\n" +
+    "<%});%>"+
+    "<%resp = '' %>"+
+    "<% _.each(data,function(value,index){%>" +
+        ".<%=resp%>p<%=index%>{ padding:<%=transform(value)%>}\n" +
+        ".<%=resp%>pt<%=index%>{ padding-top:<%=transform(value)%>}\n" +
+        ".<%=resp%>pb<%=index%>{ padding-bottom:<%=transform(value)%>}\n" +
+        ".<%=resp%>pl<%=index%>{ padding-left:<%=transform(value)%>}\n" +
+        ".<%=resp%>pr<%=index%>{ padding-right:<%=transform(value)%>}\n" +
+        ".<%=resp%>py<%=index%>{ padding-top:<%=transform(value)%>; padding-bottom:<%=transform(value)%>}\n" +
+        ".<%=resp%>px<%=index%>{ padding-left: <%=transform(value)%>; padding-right:<%=transform(value)%>}\n" +
     "<%});%>");
